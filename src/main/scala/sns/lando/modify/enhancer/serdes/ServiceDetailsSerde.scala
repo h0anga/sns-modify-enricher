f16b432b5db70def4ea321dd987839e72092d9d4
olga.bobrovnytska <olga.bobrovnytska@bskyb.com>
2019-02-27 16:15:07 +0000

    Refactor Serializer and Deserializer into same file as Serde

--- src/main/scala/sns/lando/modify/enhancer/serdes/ServiceDetailsSerde.scala
+++ src/main/scala/sns/lando/modify/enhancer/serdes/ServiceDetailsSerde.scala
@@ -21,35 +21,35 @@ class ServiceDetailsSerde extends Serde[ServiceDetails] {
   override def deserializer(): Deserializer[ServiceDetails] = {
     return new ServiceDetailsDeserializer()
   }
-
-  class ServiceDetailsSerializer() extends Serializer[ServiceDetails] {
-    val serviceDetailsParser = new ServiceDetailsParser()
-
-    override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
-    }
-
-    override def serialize(topic: String, data: ServiceDetails): Array[Byte] = {
-      serviceDetailsParser.parse(data).getBytes()
-    }
-
-    override def close(): Unit = {
-    }
-  }
-
-  class ServiceDetailsDeserializer() extends Deserializer[ServiceDetails] {
-    val serviceDetailsParser = new ServiceDetailsParser()
-
-    override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
-
-    }
-
-    override def deserialize(topic: String, data: Array[Byte]): ServiceDetails = {
-      serviceDetailsParser.parse(new String(data))
-    }
-
-    override def close(): Unit = {
-
-    }
-  }
-
 }
+
+class ServiceDetailsSerializer() extends Serializer[ServiceDetails] {
+  val serviceDetailsParser = new ServiceDetailsParser()
+
+  override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
+  }
+
+  override def serialize(topic: String, data: ServiceDetails): Array[Byte] = {
+    serviceDetailsParser.parse(data).getBytes()
+  }
+
+  override def close(): Unit = {
+  }
+}
+
+class ServiceDetailsDeserializer() extends Deserializer[ServiceDetails] {
+  val serviceDetailsParser = new ServiceDetailsParser()
+
+  override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
+
+  }
+
+  override def deserialize(topic: String, data: Array[Byte]): ServiceDetails = {
+    serviceDetailsParser.parse(new String(data))
+  }
+
+  override def close(): Unit = {
+
+  }
+}
+
--- src/main/scala/sns/lando/modify/enhancer/serdes/VoiceFeaturesSerde.scala
+++ src/main/scala/sns/lando/modify/enhancer/serdes/VoiceFeaturesSerde.scala
@@ -3,7 +3,7 @@ package sns.lando.modify.enhancer.serdes
 import java.util
 
 import org.apache.kafka.common.serialization.{Deserializer, Serde, Serializer}
-import sns.lando.modify.enhancer.VoiceFeatures
+import sns.lando.modify.enhancer.{VoiceFeatures, VoiceFeaturesParser}
 
 class VoiceFeaturesSerde extends Serde[VoiceFeatures] {
   override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
@@ -20,5 +20,35 @@ class VoiceFeaturesSerde extends Serde[VoiceFeatures] {
 
   override def deserializer(): Deserializer[VoiceFeatures] = {
     return new VoiceFeaturesDeserializer()
+  }
+}
+
+class VoiceFeaturesSerializer extends Serializer[VoiceFeatures] {
+  val voiceFeaturesParser = new VoiceFeaturesParser()
+  override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
+
+  }
+
+  override def serialize(topic: String, voiceFeatures: VoiceFeatures): Array[Byte] = {
+    return voiceFeaturesParser.parse(voiceFeatures).getBytes
+  }
+
+  override def close(): Unit = {
+
+  }
+}
+
+class VoiceFeaturesDeserializer extends Deserializer[VoiceFeatures] {
+  val voiceFeaturesParser = new VoiceFeaturesParser()
+  override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
+
+  }
+
+  override def deserialize(topic: String, data: Array[Byte]): VoiceFeatures = {
+    return voiceFeaturesParser.parse(new String(data))
+  }
+
+  override def close(): Unit = {
+
   }
 }
